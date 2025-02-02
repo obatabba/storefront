b14595d47eb1c2ec96eed2f424673ef67261320f
Obada Tabbaa <obatabba@gmail.com>
2025-02-03 02:49:33 +0300

    Fix bad validator.

--- store/validators.py
+++ store/validators.py
@@ -2,7 +2,7 @@ from django.core.exceptions import ValidationError
 
 
 def validate_file_size(file):
-    MAX_SIZE_KB = 50
+    MAX_SIZE_KB = 500
 
-    if file.size * 1024 > MAX_SIZE_KB:
+    if file.size > MAX_SIZE_KB * 1024:
         raise ValidationError(f'Files cannot be larger than {MAX_SIZE_KB}KB!')
