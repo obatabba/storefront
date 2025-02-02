0e205c5a5b1ec6ab074afc3a6f83cb86b87605ab
Obada Tabbaa <obatabba@gmail.com>
2025-02-03 02:37:58 +0300

    Create seed_db command.

---
+++ store/management/commands/seed_db.py
@@ -0,0 +1,20 @@
+from django.core.management import BaseCommand
+from django.db import connection
+from pathlib import Path
+import os
+
+
+class Command(BaseCommand):
+    help = "Populate the database with collections and products."
+
+    def handle(self, *args, **options):
+        SEED_FILE = 'seed.sql'
+
+        print('Populating the database...')
+        current_dir = os.path.dirname(__file__)
+        seed_file_path = os.path.join(current_dir, SEED_FILE)
+        sql = Path(seed_file_path).read_text()
+
+        with connection.cursor() as cursor:
+            cursor.executescript(sql)
+        print('Done.')
