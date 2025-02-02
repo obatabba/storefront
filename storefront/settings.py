3e04a8546de4377abf4113c2ed537b65fe641873
Obada Tabbaa <obatabba@gmail.com>
2025-02-03 01:56:37 +0300

    My initial commit.

--- storefront/settings.py
+++ storefront/settings.py
@@ -92,14 +92,10 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'storefront2',
-        'HOST': 'localhost',
-        'USER': 'root',
-        'PASSWORD': 'MyPassword'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
