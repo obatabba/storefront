db33c1da536aff1ef5b86add5be01266ff96d262
Obada Tabbaa <obatabba@gmail.com>
2025-02-05 03:52:43 +0300

    Configure email backend and send a test email.

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,12 @@
+from django.core.mail import EmailMessage, BadHeaderError
 from django.shortcuts import render
 
 
 def say_hello(request):
+    try:
+        email = EmailMessage('subject', 'message', 'from@storefront.com', ['john.smith@domain.com'])
+        email.attach_file('playground/static/images/Me.jpg')
+        email.send()
+    except BadHeaderError:
+        pass
     return render(request, 'hello.html', {'name': 'Mosh'})
--- storefront/settings.py
+++ storefront/settings.py
@@ -164,3 +164,14 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT',),
     'ACCESS_TOKEN_LIFETIME': timedelta(days=1)
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'localhost'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 25
+DEFAULT_FROM_EMAIL = 'from@storefront.com'
+
+ADMINS = [
+    ('Obada', 'admin@storefront.com')
+]
